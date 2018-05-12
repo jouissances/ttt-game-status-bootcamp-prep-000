6ac80aa8f21051c2bc845e79a5392924d2e45a96
Janice Darikho <janicedarikho@gmail.com>
2018-05-12 07:47:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -152,7 +152,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if full?(board) && !won?(board)
+  if full?(board) || !won?(board) || draw?(board)
     return true
   elsif full?(board) == true
     return true
